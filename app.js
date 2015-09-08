986e651ff6eed07604b8a1160e88bbbce9a022b4
Vas Leckas <vas.leckas@gmail.com>
2015-09-07 19:33:18 -0500

    update

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var passport = require('passport');
 
 var session = require('express-session');
 
-var localStrategy = require('passport-local').Strategy;
 
 var mongoose = require('mongoose');
 
@@ -34,6 +35,10 @@ MongoDB.once('open', function () {
 });
 
 // view engine setup
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -42,11 +47,8 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(cookieParser());
 
 app.use(session({
     secret: 'secret',
